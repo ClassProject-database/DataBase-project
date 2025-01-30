9c6d58b98a7091b0973778936e0c5f908127d54d
Matthewh1225 <mhuckman97@yahoo.com>
2025-01-30 17:31:40 -0600

    added to nav bar

--- DataBase project/website/auth.py
+++ DataBase project/website/auth.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
  
 auth=Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return '<p> login </p>'
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
@@ -14,3 +15,7 @@ def logout():
 @auth.route('/sign-up')
 def sign_up():
     return '<p> sign up </p>'
+
+@auth.route('/inventory')
+def inventory_button():
+    return '<p> inventory </p>'
