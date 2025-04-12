8078090bbf5bbb1153fcbdc3789f0556fb99c4bd
Matthewh1225 <mhuckman97@yahoo.com>
2025-04-11 21:03:00 -0500

    change

--- Website/Backend/main.py
+++ Website/Backend/main.py
@@ -4,6 +4,6 @@ import os
 
 app = create_app()
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
-
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
