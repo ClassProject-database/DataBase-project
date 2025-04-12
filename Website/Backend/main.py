0654a24bf750bb9d319b9bd318146cdbd9ec0a07
Matthewh1225 <mhuckman97@yahoo.com>
2025-04-11 22:11:35 -0500

    main.py

--- Website/Backend/main.py
+++ Website/Backend/main.py
@@ -1,5 +1,9 @@
-# main.py
 from Website.Backend import create_app
+import os
 
 app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
+    app.run(host="0.0.0.0", port=port, debug=True)  # required for Render
 
